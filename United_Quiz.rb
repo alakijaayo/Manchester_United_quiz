e865ba2896cd5cff8cfd481d05b57b80809f9327
alakijayo <alakijaayo@gmail.com>
2018-09-26 11:09:54 +0100

    level selection

--- United_Quiz.rb
+++ United_Quiz.rb
@@ -1,7 +1,18 @@
 require 'sinatra/base'
 
 class Quiz < Sinatra::Base
+  enable :sessions
   get '/' do
     erb :homepage
+  end
+
+  post '/level' do
+    session[:level] = params[:level]
+    redirect '/start'
+  end
+
+  get '/start' do
+    @level = session[:level]
+    erb :choice
   end
 end
--- spec/features/United_Quiz_spec.rb
+++ spec/features/United_Quiz_spec.rb
@@ -3,4 +3,25 @@ feature 'United Quiz' do
     visit('/')
     expect(page).to have_content "Hello and welcome to the Manchester United Quiz!"
   end
+
+  scenario 'it allows you to pick easy questions' do
+    visit('/')
+    select 'Easy', from: 'level'
+    click_button 'Submit'
+    expect(page).to have_content "You have selected the Easy questions."
+  end
+
+  scenario 'it allows you to pick medium questions' do
+    visit('/')
+    select 'Medium', from: 'level'
+    click_button 'Submit'
+    expect(page).to have_content "You have selected the Medium questions."
+  end
+
+  scenario 'it allows you to pick hard questions' do
+    visit('/')
+    select 'Hard', from: 'level'
+    click_button 'Submit'
+    expect(page).to have_content "You have selected the Hard questions. Only for the Die Hard Red Devils!!"
+  end
 end
